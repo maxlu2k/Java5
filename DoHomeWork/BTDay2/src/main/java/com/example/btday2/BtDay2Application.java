2871aa5a85e09cb9f956fd2314c4c0d474a83d1d
maxlu2k <alvinnguyen983@gmail.com>
2023-05-11 15:18:20 +0700

    bài tập day2

---
+++ DoHomeWork/BTDay2/src/main/java/com/example/btday2/BtDay2Application.java
@@ -0,0 +1,13 @@
+package com.example.btday2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtDay2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtDay2Application.class, args);
+    }
+
+}
