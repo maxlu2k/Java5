b4c4645fe4a0312a0d568fbeaf8ea6c08355fe5e
maxlu2k <alvinnguyen983@gmail.com>
2023-05-22 10:36:55 +0700

    lab

---
+++ LAB/SOF302_PhongTT35_MSSV_Ten_hi_01/src/main/java/com/poly/tiennh21/ph19187/Sof302PhongTt35MssvTenHi01Application.java
@@ -0,0 +1,13 @@
+package com.poly.tiennh21.ph19187;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sof302PhongTt35MssvTenHi01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sof302PhongTt35MssvTenHi01Application.class, args);
+    }
+
+}
---
+++ LAB/SOF302_PhongTT35_MSSV_Ten_hi_01/src/test/java/com/poly/tiennh21/ph19187/Sof302PhongTt35MssvTenHi01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.tiennh21.ph19187;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sof302PhongTt35MssvTenHi01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
