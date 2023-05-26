a7ff08169eb72db7ef551e137923c091e80fd8bf
maxlu2k <alvinnguyen983@gmail.com>
2023-05-26 10:17:46 +0700

    new

---
+++ DemoJava5D9/src/main/java/com/example/demojava5d9/DemoJava5D9Application.java
@@ -0,0 +1,13 @@
+package com.example.demojava5d9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJava5D9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJava5D9Application.class, args);
+    }
+
+}
---
+++ DemoJava5D9/src/test/java/com/example/demojava5d9/DemoJava5D9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojava5d9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJava5D9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
